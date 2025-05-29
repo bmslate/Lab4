86086237b5b0791bb4f5196114588ddde49e6a68
Huijun Bu <bmslate2022@gmail.com>
2025-05-30 01:19:52 +0800

    First Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Lab4.Models;
+
+namespace Lab4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Welcome with ViewBag";
+            ViewData["Title"] = "Welcome with ViewData";
+            
+            return View();
+        }
+
+        public IActionResult SongForm() => View();
+        [HttpPost]
+        public IActionResult Sing(int monkeyCount)
+        {
+            ViewData["Title"] = "Sing";
+            if (monkeyCount < 20 || monkeyCount > 30)
+            {
+                return RedirectToAction("SongForm");
+            }
+
+            ViewBag.MonkeyCount = monkeyCount;
+            return View();
+        }
+        //public IActionResult Sing()
+        //{
+        //    ViewData["Title"] = "Sing";
+        //    // you will complete this
+        //    return View();
+        //}
+        public IActionResult CreateStudent() => View();
+        [HttpPost]
+        public IActionResult DisplayStudent(Student student)
+        {
+            // you will complete this
+            return View(student);
+        }
+        public IActionResult Error()
+        {
+            ViewData["Error"] = "Error message";
+            return View();
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,19 @@
+﻿namespace Lab4.Models
+{
+     
+    public class Student
+        {
+            //public StudentInfo() { }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string EmailAddress { get; set; }
+            public int StudentId { get; set; }
+            public string PassWord { get; set; }
+            public string Description { get; set; }
+
+        
+
+
+        }
+    
+}
